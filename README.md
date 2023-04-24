d62d8ac9379b4a8c1d23f36fe3cc29a96d253ecc
Matt Meyer <mattjacobnyc@gmail.com>
2023-04-24 18:24:46 -0400

    updates made to read me, and final touches added

--- README.md
+++ README.md
@@ -1,34 +1,64 @@
-## Matt Meyer project notes
+# Matt Meyer
 
--General Notes:
---since feedback was provided that using a framework for this project would be a nice to have,
-certain, I decided to work with Next.js to challenge myself.
+### _King & Partners Code Assessment_
+
+---
+
+#### to load project:
+
+clone or downlaod the Github repo I have stored the code in, at this url - **https://github.com/matt-meyer-nyc/king-and-partners-code-assessment**
+
+From root directory of project in terminal
+run '**npm install**'
+
+once installed, run
+run '**npm run dev**'
+
+project should load locally on
+http://localhost:3000/
+
+default Next.js boilerplate instructions also included at bottom of this file
+
+---
+
+## PROJECT NOTES
+
+### General Notes:
+
+--since feedback was provided that using a framework for this project would be a nice-to-have, I decided to work with Next.js to challenge myself (in my prior interviews I've discussed that working with JS is something I'm learning about every day, and I'm passionate about growing as a developer utilizing this framework with WP)
 Next configurations have been incorporated:
 -- project utilizes next/font utilized (in place of static Google font files provided via link to use as CDN); this approach adds optimization since font file doesn't need to be called repeatedly
 -- given that best practice would be to scope the CSS to each component, my overall styling approach utilizes CSS modules; however in globals.css, towards the bottom, I have included BEM CSS (per the project requirements) related to the accordion component to demonstrate knowledge about this CSS approach (and for this component utilized dynamic css class prefixes appended to the classes in BEM style)
 
--Assumptions being made
---hero h1 is relative of other headings, and therefore, as specified with all other headings, line-spacing should be set to -2%
---assumption being made that opening and closing of nav should be tied to the tab with the paw on it (and close would not be triggered by clicking background of entire nav modal)
+### Assumptions being made
 
--Outstanding questions/concerns
---very much appears that, Intro and Accordion components sit within a 2 column layout that should stack to 1 column in mobile. However, the heading (desktop) for Intro is broken into 2 separate text boxes in Figment design (i.e. it is not 1 whole heading), and similarly the headings for Accordion (desktop, tablet) are broken into two separate text boxes. This is inconsistent with the mobile design where all headings are 1 consistent text box in Figma. Though it possible to workaround this injecting HTML via JavaScript, this is highly atypical, and not best practice, therefore it is uncertain as to whether this is possibly a Figment error, or an intentional decision. If it's intentional, I'm happy to discuss workarounds in more detail (as an example, for Intro desktop, there is a possible insination this text is being floated, however, this would be illogical if the component is intended to utilize a 2 column layout)
+--hero h1 is a relative of all other headings, and therefore, as specified with all other headings, line-spacing should be set to -2%
+--assumption being made that opening and closing of nav should be tied to the tab with the paw on it (and close would not be triggered by clicking background of entire nav modal)
+--wasn't sure which way this should go, so made decision to have the accordion image dictate the size of the accordion (could easily be swapped such that the accordion container recievees precise dimension to dictate the size of the container instead)
+
+### Outstanding questions/concerns
+
+--very much appears that, Intro and Accordion components sit within a 2 column layout that should stack to 1 column in mobile. However, the heading (desktop) for Intro is broken into 2 separate text boxes in Figment design (i.e. it is not 1 whole heading), and similarly the headings for Accordion (desktop, tablet) are broken into two separate text boxes. This is inconsistent with the mobile design where all headings are 1 consistent text box in Figma. Though it possible to workaround this injecting HTML via JavaScript, this is highly atypical, and not best practice, therefore it is uncertain as to whether this is possibly a Figment error, or an intentional decision. If it's intentional, I'm happy to discuss workarounds in more detail (as an example, for Intro desktop, there is a possible insinuation this text is being floated, however, this would be illogical if the component is intended to utilize a 2 column layout)
 --not sure why certain h1, h2 are decimals and not integers in Figma (ex. line-height)
---the padding for the button was inconsitent with centering the text (ex. in mobile the top padding is 15px and bottom is 14px), so decicion made to keep padding even and choose one of those numbers so top/bottom padding is even, as well as left right padding
+--the padding for the button was inconsistent with centering the text (ex. in mobile the top padding is 15px and bottom is 14px), so decision made to keep padding even and choose one of those numbers so top/bottom padding is even, as well as left right padding
 --for the accordion header, the font-size (40px) did not work with the 24px padding specified, so intentional decision made to reduce font-size to 38px;
---font-size in Figma for the hero header in tablet (90px) caused line break, font-size reduced to 73px to match design (same for desktop font size reduced from 176px to 153px)
---wasn't sure how to handle the hero heading text breaking to 3 lines and the line-height specified in Figma cuasing this to make the hero text expand outside it's container (whether you might be looking for a dynamic responsive font size, or if the line-height should be reduced, etc.), I added a media query between 768px and 955px to handle this be reducing the line-height between those dimensions (and kept the text broken into 3 lines)
+--font-size in Figma for the hero header in tablet (90px) caused line break, font-size reduced to 86px to match design (same for desktop font size reduced from 176px to 172px)--tried adjusting letter-spacing to consistently be 0.02em based on parameters noted in Figma, but was complex to discern given I'm not familiar with how Mixed type works in Figma (and which one should be referenced of the Mixed details)
 --assumption being made that opening and closing of nav should be tied to the tab with the paw on it (and close would not be triggered by clicking background of entire nav modal)
 --intentionally kept tab absolutely positioned relative to the hero, though arguably fixed positioning might be desired such that a user is always able to access the nav regardless of where they are on the page
 --the ul width of the pages listed in the footer are slightly different (ex. left pages ul is 160px and right is 162px in mobile), for consistency, decision made to make the uls the same width
+--tried for a while to find the border-radius for the footer, but could not find it in Figma file, so matched it to the border radius used for the images and accordion buttons/tabs (30px)
 
--Other notes
---for simplicity sake, since this is more of a design oriented project, wrapping a tags for links (<Link> elements in Next.js) have been discluded with the expecption of the navigation modal
+### Other notes
 
---Challenging Aspects / Aspects that could be improved
+--for simplicity sake, since this is more of a design oriented project, wrapping a tags for links (<Link> elements in Next.js) have been discluded with the excption of the navigation modal
+
+## Challenging Aspects / Aspects that could be improved
+
 --for the footer, although the pages listed in the ul (under the heading) are detailed in Figma in mobile as having a line-height of 30px, when rendered, this caused way too much vertical distance between the li items. to split the difference and attempt to match the design I reduced the line-height to 15px. Despite this, the height of the main footer component (in my design) is 398px, while the design has the height at 364px; the cat's meow logo, the social logos, and the font-sizes of all the pages listed are correct, and the padding surrounding them appears to be correct, so I'm wasn't exactly sure how to resolve this.
 --for the accordion, I'm aware that the JS approach to the click handling is not DRY, and should be optimized by mapping over an array (or arrays) that hold the content for the accordion headings and accordion text. I, admittedly, could use some guidance as far as learning how to handle what I'd imagine is the equivalent to using 'this' (or something along those lines) when using .map() (having come from coding largely with jQuery prior)
---I decided to go (on the whole) with a mobile first approach, with media queries increasing as the page size increases. However, the hero required a mix of media queries and combining 'min-width' and 'max-width' media queries. I'm pretty certain there's a cleaner way to handle this, and would learn to learn more about what might be a better approach
+--I decided to go (on the whole) with a mobile first approach, with media queries increasing as the page size increases. However, the hero required a mix of media queries I'm pretty certain there's a cleaner way to handle this, and would learn to learn more about what might be a better approach
+--for desktop sizes wasn't sure how to handle the hero heading text breaking to 3 lines and the line-height specified in Figma (213px) causing this to make the hero text expand outside it's container (wasn't sure whether you might be looking for a dynamic responsive font size, or if the line-height should be reduced, etc.), I added a media query between 768px and 955px to handle this be reducing the line-height between certain dimensions (and kept the text broken into 3 lines) ...same for tablet where I added a media query between 376px and 461px to account for the text breaking to 3 lines and having too large a line height. Along these lines, in addition, I wasn't sure whether the larger text denoted in Figma for desktop should persist through the tablet breakpoint (when it approaches being closer to the tablet breakpoint, the text feel very large).
+However, conversely, if the text size for tablet persists up until the desktop breakpoint, the text feels way too small at sizes closer to desktop (1080px+ etc.)
 
 ## Nex.js boiler plate
 
--- components/accordion/accordion.js
+++ components/accordion/accordion.js
@@ -4,6 +4,7 @@ export const Accordion = () => {
   let [item1, setItem1] = useState('closed');
   let [item2, setItem2] = useState('closed');
   let [item3, setItem3] = useState('closed');
+  let [item4, setItem4] = useState('closed');
 
   const handleOnClickDiningOpen = () => {
     setItem1('open');
@@ -12,18 +13,25 @@ export const Accordion = () => {
     setItem1('closed');
   };
 
-  const handleOnClickPlayOpen = () => {
+  const handleOnClickGroomingOpen = () => {
     setItem2('open');
   };
-  const handleOnClickPlayClose = () => {
+  const handleOnClickGroomingClose = () => {
     setItem2('closed');
   };
 
-  const handleOnClickPhotoOpen = () => {
+  const handleOnClickPlayOpen = () => {
     setItem3('open');
   };
-  const handleOnClickPhotoClose = () => {
+  const handleOnClickPlayClose = () => {
     setItem3('closed');
+  };
+
+  const handleOnClickPhotoOpen = () => {
+    setItem4('open');
+  };
+  const handleOnClickPhotoClose = () => {
+    setItem4('closed');
   };
 
   return (
@@ -32,7 +40,10 @@ export const Accordion = () => {
         <div className="accordion-col-one">
           <h3>Pamper your cat to Purrrrrrrrrrrfection</h3>
           <div className="accordion-holder">
-            <div class="accordion-heading" onClick={handleOnClickDiningOpen}>
+            <div
+              className={`accordion-heading accordion-heading--${item1}`}
+              onClick={handleOnClickDiningOpen}
+            >
               <span>Dining</span>
               <div>
                 <svg
@@ -48,14 +59,14 @@ export const Accordion = () => {
                     plus symbol to toggle open accordion item
                   </label>
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M7.7813 6.5835e-07C8.19551 0.000543537 8.53085 0.33677 8.53031 0.750983L8.51196 14.751C8.51142 15.1652 8.17519 15.5005 7.76098 15.5C7.34677 15.4995 7.01142 15.1632 7.01196 14.749L7.03031 0.749017C7.03086 0.334804 7.36708 -0.00054228 7.7813 6.5835e-07Z"
                     fill="#0B5841"
                   />
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M0 7.75C0 7.33579 0.335786 7 0.75 7H14.75C15.1642 7 15.5 7.33579 15.5 7.75C15.5 8.16421 15.1642 8.5 14.75 8.5H0.75C0.335786 8.5 0 8.16421 0 7.75Z"
                     fill="#0B5841"
                   />
@@ -67,7 +78,7 @@ export const Accordion = () => {
               onClick={handleOnClickDiningClose}
             >
               <div>
-                <span>Grooming and Spa Services</span>
+                <span>Dining</span>
                 <div>
                   <svg
                     width="16"
@@ -76,13 +87,14 @@ export const Accordion = () => {
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                     aria-labelledby="minus-icon"
+                    role="button"
                   >
                     <label id="minus-icon">
                       Minus icon to close accordion item
                     </label>
                     <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
                       d="M0 0.75C0 0.335786 0.335786 0 0.75 0H14.75C15.1642 0 15.5 0.335786 15.5 0.75C15.5 1.16421 15.1642 1.5 14.75 1.5H0.75C0.335786 1.5 0 1.16421 0 0.75Z"
                       fill="white"
                     />
@@ -103,7 +115,86 @@ export const Accordion = () => {
                 best during their stay at the luxury cat resort.",
               </span>
             </div>
-            <div class="accordion-heading" onClick={handleOnClickPlayOpen}>
+            <div
+              className={`accordion-heading accordion-heading--${item2}`}
+              onClick={handleOnClickGroomingOpen}
+            >
+              <span>Grooming and Spa Services</span>
+              <div>
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-labelledby="plus-icon"
+                  role="button"
+                >
+                  <label id="plus-icon">
+                    plus symbol to toggle open accordion item
+                  </label>
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M7.7813 6.5835e-07C8.19551 0.000543537 8.53085 0.33677 8.53031 0.750983L8.51196 14.751C8.51142 15.1652 8.17519 15.5005 7.76098 15.5C7.34677 15.4995 7.01142 15.1632 7.01196 14.749L7.03031 0.749017C7.03086 0.334804 7.36708 -0.00054228 7.7813 6.5835e-07Z"
+                    fill="#0B5841"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M0 7.75C0 7.33579 0.335786 7 0.75 7H14.75C15.1642 7 15.5 7.33579 15.5 7.75C15.5 8.16421 15.1642 8.5 14.75 8.5H0.75C0.335786 8.5 0 8.16421 0 7.75Z"
+                    fill="#0B5841"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div
+              className={`accordion-text--${item2}`}
+              onClick={handleOnClickGroomingClose}
+            >
+              <div>
+                <span>Grooming and Spa Services</span>
+                <div>
+                  <svg
+                    width="16"
+                    height="2"
+                    viewBox="0 0 16 2"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-labelledby="minus-icon"
+                    role="button"
+                  >
+                    <label id="minus-icon">
+                      Minus icon to close accordion item
+                    </label>
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M0 0.75C0 0.335786 0.335786 0 0.75 0H14.75C15.1642 0 15.5 0.335786 15.5 0.75C15.5 1.16421 15.1642 1.5 14.75 1.5H0.75C0.335786 1.5 0 1.16421 0 0.75Z"
+                      fill="white"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <span>
+                [Grooming and Spa Services] Grooming services include
+                professional bathing, brush-outs, nail trims, ear cleaning, and
+                even fur styling. The resort also offers spa services such as
+                massages, facials, and aromatherapy treatments to pamper the
+                cats. Grooming and spa services are designed to enhance the
+                cat's physical appearance and overall well-being. Additionally,
+                the resort provides personalized grooming services tailored to
+                the specific needs of each cat, such as specialized shampoos for
+                sensitive skin or hypoallergenic grooming products for cats with
+                allergies. With professional grooming services, cats can look
+                and feel their best during their stay at the luxury cat
+                resort.",
+              </span>
+            </div>
+            <div
+              className={`accordion-heading accordion-heading--${item3}`}
+              onClick={handleOnClickPlayOpen}
+            >
               <span>Playtime and Enrichment</span>
               <div>
                 <svg
@@ -119,14 +210,14 @@ export const Accordion = () => {
                     plus symbol to toggle open accordion item
                   </label>
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M7.7813 6.5835e-07C8.19551 0.000543537 8.53085 0.33677 8.53031 0.750983L8.51196 14.751C8.51142 15.1652 8.17519 15.5005 7.76098 15.5C7.34677 15.4995 7.01142 15.1632 7.01196 14.749L7.03031 0.749017C7.03086 0.334804 7.36708 -0.00054228 7.7813 6.5835e-07Z"
                     fill="#0B5841"
                   />
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M0 7.75C0 7.33579 0.335786 7 0.75 7H14.75C15.1642 7 15.5 7.33579 15.5 7.75C15.5 8.16421 15.1642 8.5 14.75 8.5H0.75C0.335786 8.5 0 8.16421 0 7.75Z"
                     fill="#0B5841"
                   />
@@ -134,11 +225,11 @@ export const Accordion = () => {
               </div>
             </div>
             <div
-              className={`accordion-text--${item2}`}
+              className={`accordion-text--${item3}`}
               onClick={handleOnClickPlayClose}
             >
               <div>
-                <span>Playtime and enrichment</span>
+                <span>Playtime and Enrichment</span>
                 <div>
                   <svg
                     width="16"
@@ -147,13 +238,14 @@ export const Accordion = () => {
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                     aria-labelledby="minus-icon"
+                    role="button"
                   >
                     <label id="minus-icon">
                       Minus icon to close accordion item
                     </label>
                     <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
                       d="M0 0.75C0 0.335786 0.335786 0 0.75 0H14.75C15.1642 0 15.5 0.335786 15.5 0.75C15.5 1.16421 15.1642 1.5 14.75 1.5H0.75C0.335786 1.5 0 1.16421 0 0.75Z"
                       fill="white"
                     />
@@ -161,7 +253,7 @@ export const Accordion = () => {
                 </div>
               </div>
               <span>
-                [Playtime and enrichment] Grooming services include professional
+                [Playtime and Enrichment] Grooming services include professional
                 bathing, brush-outs, nail trims, ear cleaning, and even fur
                 styling. The resort also offers spa services such as massages,
                 facials, and aromatherapy treatments to pamper the cats.
@@ -175,7 +267,10 @@ export const Accordion = () => {
                 resort.",
               </span>
             </div>
-            <div class="accordion-heading" onClick={handleOnClickPhotoOpen}>
+            <div
+              className={`accordion-heading accordion-heading--${item4}`}
+              onClick={handleOnClickPhotoOpen}
+            >
               <span>Pet Photography and Social Media</span>
               <div>
                 <svg
@@ -191,14 +286,14 @@ export const Accordion = () => {
                     plus symbol to toggle open accordion item
                   </label>
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M7.7813 6.5835e-07C8.19551 0.000543537 8.53085 0.33677 8.53031 0.750983L8.51196 14.751C8.51142 15.1652 8.17519 15.5005 7.76098 15.5C7.34677 15.4995 7.01142 15.1632 7.01196 14.749L7.03031 0.749017C7.03086 0.334804 7.36708 -0.00054228 7.7813 6.5835e-07Z"
                     fill="#0B5841"
                   />
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M0 7.75C0 7.33579 0.335786 7 0.75 7H14.75C15.1642 7 15.5 7.33579 15.5 7.75C15.5 8.16421 15.1642 8.5 14.75 8.5H0.75C0.335786 8.5 0 8.16421 0 7.75Z"
                     fill="#0B5841"
                   />
@@ -206,7 +301,7 @@ export const Accordion = () => {
               </div>
             </div>
             <div
-              className={`accordion-text--${item3}`}
+              className={`accordion-text--${item4}`}
               onClick={handleOnClickPhotoClose}
             >
               <div>
@@ -219,13 +314,14 @@ export const Accordion = () => {
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                     aria-labelledby="minus-icon"
+                    role="button"
                   >
                     <label id="minus-icon">
                       Minus icon to close accordion item
                     </label>
                     <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
                       d="M0 0.75C0 0.335786 0.335786 0 0.75 0H14.75C15.1642 0 15.5 0.335786 15.5 0.75C15.5 1.16421 15.1642 1.5 14.75 1.5H0.75C0.335786 1.5 0 1.16421 0 0.75Z"
                       fill="white"
                     />
